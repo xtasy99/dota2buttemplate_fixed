f0518c6e48466596f3bcef32fb78a8ea8f826415
Snoresville <reflectiveflawless@hotmail.com>
2021-04-12 08:03:18 +0930

    Added bots setting and commented legacy code

--- game/dota_addons/addon_template_butt/scripts/vscripts/internal/init.lua
+++ game/dota_addons/addon_template_butt/scripts/vscripts/internal/init.lua
@@ -1,3 +1,5 @@
+--[[ Legacy Code
+
 if IsInToolsMode() then
 	function IsSchokokeks()
 		return tostring(PlayerResource:GetSteamID(0))=="76561198007073158"
@@ -30,3 +32,5 @@ if IsInToolsMode() then
 	-- print(fileToString("http://github.com/SteamDatabase/GameTracking-Dota2/blob/master/game/dota/pak01_dir/scripts/npc/items.txt"))
 
 end
+
+]]
