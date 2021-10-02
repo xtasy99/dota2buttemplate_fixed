1aeccbcec3d610472330c1632feb0bdcd591f408
Snoresville <reflectiveflawless@hotmail.com>
2021-10-03 01:07:43 +0930

    Added a python file that can let you manually update your dota kv to the latest version
    this is cool because you wont have to wait for me to update this template to include the new neutrals, i've just used it and i didn't have to copy and paste stuff lol

---
+++ game/dota_addons/addon_template_butt/scripts/__update_kv.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+files = [
+    ["npc\\_abilities.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/scripts/npc/npc_abilities.txt"],
+    ["npc\\_heroes.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/scripts/npc/npc_heroes.txt"],
+    ["npc\\_items.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/items.txt"],
+    ["npc\\_neutral_items.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/neutral_items.txt"],
+    ["npc\\npc_neutral_items_custom.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/neutral_items.txt"],
+    ["npc\\_units.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/scripts/npc/npc_units.txt"],
+
+    ["shops\\dota_fixed_shops.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/83ab42f2988545cc3ae00eeedfcd0d00ff35c3b9/game/dota/pak01_dir/scripts/shops.txt"],
+]
+
+for filetype in files:
+    r = requests.get(filetype[1], allow_redirects=True)
+    open(os.path.realpath(__file__) + "\\..\\" + filetype[0], 'wb').write(r.content)
+
