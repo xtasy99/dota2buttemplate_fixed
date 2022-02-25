2672f315d9d14af2338c785fcf3443f0cf9e3466
Snoresville <reflectiveflawless@hotmail.com>
2022-02-25 10:52:33 +1030

    updated the kv updater

--- game/dota_addons/addon_template_butt/scripts/__update_kv.py
+++ game/dota_addons/addon_template_butt/scripts/__update_kv.py
@@ -2,11 +2,11 @@ import requests
 import os
 
 files = [
-    ["npc\\_abilities.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/scripts/npc/npc_abilities.txt"],
-    ["npc\\_heroes.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/scripts/npc/npc_heroes.txt"],
+    ["npc\\_abilities.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_abilities.txt"],
+    ["npc\\_heroes.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_heroes.txt"],
     ["npc\\_items.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/items.txt"],
     ["npc\\_neutral_items.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/neutral_items.txt"],
-    ["npc\\_units.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/scripts/npc/npc_units.txt"],
+    ["npc\\_units.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_units.txt"],
 
     ["npc\\herolist.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/activelist.txt"],
     ["npc\\npc_neutral_items_custom.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/neutral_items.txt"],
