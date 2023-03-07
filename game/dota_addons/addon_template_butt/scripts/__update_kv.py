0d54352241a18705a828e90e122c15e152782452
Snoresville <reflectiveflawless@hotmail.com>
2023-03-07 17:39:26 +1030

    Patch 7.32e

--- game/dota_addons/addon_template_butt/scripts/__update_kv.py
+++ game/dota_addons/addon_template_butt/scripts/__update_kv.py
@@ -2,16 +2,16 @@ import requests
 import os
 
 files = [
-    ["npc\\_abilities.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_abilities.txt"],
-    ["npc\\_heroes.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_heroes.txt"],
-    ["npc\\_items.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/items.txt"],
-    ["npc\\_neutral_items.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/neutral_items.txt"],
-    ["npc\\_units.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_units.txt"],
+    ["npc\\_abilities.txt", "https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/npc_abilities.txt"],
+    ["npc\\_heroes.txt", "https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/npc_heroes.txt"],
+    ["npc\\_items.txt", "https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/items.txt"],
+    ["npc\\_neutral_items.txt", "https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/neutral_items.txt"],
+    ["npc\\_units.txt", "https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/npc_units.txt"],
 
     ["npc\\herolist.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/activelist.txt"],
-    ["npc\\npc_neutral_items_custom.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/neutral_items.txt"],
+    ["npc\\npc_neutral_items_custom.txt", "https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/neutral_items.txt"],
 
-    ["shops\\dota_fixed_shops.txt", "https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/shops.txt"],
+    ["shops\\dota_fixed_shops.txt", "https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/shops.txt"],
 ]
 
 for filetype in files:
